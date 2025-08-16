876779694fda3fe61419812c5f1ec391f8a98569
Michael Ernst <mernst@cs.washington.edu>
2025-08-16 12:00:33 -0700

    Test memory usage

--- src/test/java/org/plumelib/javacparse/JavacParseTest.java
+++ src/test/java/org/plumelib/javacparse/JavacParseTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.IOException;
 import org.junit.jupiter.api.Test;
+import org.plumelib.util.SystemPlume;
 
 class JavacParseTest {
   @Test
@@ -14,5 +15,15 @@ class JavacParseTest {
     assertFalse(r1.hasParseError());
     JavacParseResult r2 = JavacParse.parseJavaCode("class SyntaxError { void () {} }");
     assertTrue(r2.hasParseError());
+  }
+
+  @Test
+  void memoryTest() {
+    long initialUsedMemory = SystemPlume.usedMemory(true);
+    for (int i = 0; i < 1_000_000; i++) {
+      JavacParse.parseJavaCode("class MyClass { void m() {} }");
+    }
+    long finalUsedMemory = SystemPlume.usedMemory(true);
+    assertTrue((float) finalUsedMemory / (float) initialUsedMemory < 1.02);
   }
 }
