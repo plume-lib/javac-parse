7135b0f427444ef3c1b845028afafb01c9a969d5
Michael Ernst <mernst@cs.washington.edu>
2025-08-17 16:28:30 -0700

    Parse expressions too

--- src/main/java/org/plumelib/javacparse/JavacParse.java
+++ src/main/java/org/plumelib/javacparse/JavacParse.java
@@ -29,10 +29,10 @@ public final class JavacParse {
    * Parse a Java file.
    *
    * @param filename the file to parse
-   * @return a (parsed) compilation unit, or null if there is a parse error
+   * @return a (parsed) compilation unit
    * @throws IOException if there is trouble reading the file
    */
-  public static JavacParseResult parseJavaFile(String filename) throws IOException {
+  public static JavacParseResult<JCCompilationUnit> parseFile(String filename) throws IOException {
     return parseJavaFileObject(new FileJavaFileObject(filename));
   }
 
@@ -40,13 +40,93 @@ public final class JavacParse {
    * Parse a Java file's contents.
    *
    * @param javaCode the contents of a Java file
-   * @return a (parsed) compilation unit, or null if there is a parse error
+   * @return a (parsed) compilation unit
    */
-  public static JavacParseResult parseJavaCode(String javaCode) {
+  public static JavacParseResult<CompilationUnit> parseCompilationUnit(String javaCode) {
     try {
       return parseJavaFileObject(new StringJavaFileObject(javaCode));
     } catch (IOException e) {
       throw new Error("This can't happen", e);
+    }
+  }
+
+  /**
+   * Parses the given Java class.
+   *
+   * @param expressionSource the string representation of a Java expression
+   * @return the parsed expression
+   */
+  public static JavacParseResult<ClassTree> parseClass(String classSource) {
+    JavacParseResult<CompilationUnit> parsedFile = parseJavaFileContents(classSource);
+
+    // TODO
+  }
+
+  /**
+   * Parses the given Java method.
+   *
+   * @param expressionSource the string representation of a Java expression
+   * @return the parsed expression
+   */
+  public static JavacParseResult<MethodTree> parseMethod(String methodSource) {
+    // TODO
+  }
+
+  /**
+   * Parses the given Java expression string, such as "foo.bar()" or "1 + 2"
+   *
+   * @param expressionSource the string representation of a Java expression
+   * @return the parsed expression
+   */
+  public static JavacParseResult<ExpressionTree> parseExpression(String expressionSource) {
+
+    String dummySource = "class ParseExpression { Object expression = " + expressionSource + "; }";
+
+    JavaFileObject fileObject =
+        new SimpleJavaFileObject(
+            URI.create("string:///ParseExpression.java"), JavaFileObject.Kind.SOURCE) {
+          @Override
+          public CharSequence getCharContent(boolean ignoreEncodingErrors) {
+            return dummySource;
+          }
+        };
+
+    DiagnosticCollector<JavaFileObject> diags = new DiagnosticCollector<>();
+
+    // Prepare the file manager and task
+    try {
+      JavacTask task =
+          (JavacTask)
+              javaCompiler.getTask(
+                  null,
+                  fileManager,
+                  diags,
+                  Collections.emptyList(),
+                  null,
+                  Collections.singletonList(fileObject));
+
+      // Parse the source and extract the CompilationUnit
+      CompilationUnitTree cu = task.parse().iterator().next();
+
+      for (Diagnostic<? extends JavaFileObject> d : diags.getDiagnostics()) {
+        if (d.getKind() == Diagnostic.Kind.ERROR) {
+          throw new RuntimeException("Expression is not valid: " + d.getMessage(null));
+        }
+      }
+
+      // Get the first member (the dummy field) from the ClassTree and cast to VariableTree
+      ClassTree classTree = (ClassTree) cu.getTypeDecls().get(0);
+      VariableTree varTree = (VariableTree) classTree.getMembers().get(0);
+
+      ExpressionTree expr = varTree.getInitializer();
+      if (expr == null) {
+        throw new RuntimeException("Expression not found in AST.");
+      }
+
+      return expr;
+
+    } catch (IOException | IndexOutOfBoundsException | ClassCastException e) {
+      throw new RuntimeException("Expression parsing failed", e);
     }
   }
 
@@ -57,7 +137,8 @@ public final class JavacParse {
    * @return a compilation unit and the parse errors encountered in it
    * @throws IOException if there is trouble reading the file
    */
-  public static JavacParseResult parseJavaFileObject(JavaFileObject source) throws IOException {
+  public static JavacParseResult<JCCompilationUnit> parseJavaFileObject(JavaFileObject source)
+      throws IOException {
     // Per the documentation of Context, "a single Context is used for each invocation of the
     // compiler".
     Context context = new Context();
