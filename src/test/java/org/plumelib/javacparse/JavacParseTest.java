fd4e280c04a486e3a1913ef41a551e3448a85f4a
Michael Ernst <mernst@cs.washington.edu>
2025-08-16 18:07:48 -0700

    Expand memory test

--- src/test/java/org/plumelib/javacparse/JavacParseTest.java
+++ src/test/java/org/plumelib/javacparse/JavacParseTest.java
@@ -3,6 +3,7 @@ package org.plumelib.javacparse;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.IOException;
+import java.util.StringJoiner;
 import org.junit.jupiter.api.Test;
 import org.plumelib.util.SystemPlume;
 
@@ -19,11 +20,28 @@ class JavacParseTest {
 
   @Test
   void memoryTest() {
-    long initialUsedMemory = SystemPlume.usedMemory(true);
-    for (int i = 0; i < 1_000_000; i++) {
-      JavacParse.parseJavaCode("class MyClass { void m() {} }");
+    double initialUsedMemory = (double) SystemPlume.usedMemory(true);
+    for (int i = 0; i < 100; i++) {
+      for (int j = 0; j < 1000; j++) {
+        // Make a new String each time through the loop.
+        StringJoiner sj = new StringJoiner(System.lineSeparator());
+        for (int k = 0; k < 1000; k++) {
+          sj.add("class MyClass" + k + " { void m() {} }");
+        }
+        JavacParse.parseJavaCode(sj.toString());
+      }
+      String msg = SystemPlume.gcUsageMessage(.5, 15);
+      if (msg != null) {
+        System.out.println(msg);
+      }
     }
-    long finalUsedMemory = SystemPlume.usedMemory(true);
-    assertTrue((float) finalUsedMemory / (float) initialUsedMemory < 1.02);
+    double finalUsedMemory = (double) SystemPlume.usedMemory(true);
+    if (finalUsedMemory / initialUsedMemory > 1.02) {
+      throw new Error(
+          "initial used memory = "
+              + initialUsedMemory
+              + ", final used memory = "
+              + finalUsedMemory);
+    }
   }
 }
